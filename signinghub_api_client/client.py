5f56c9baea3098e339437651238e5e889cc88b75
Michaël Dierick <michael.dierick@redpencil.io>
2021-01-11 20:01:25 +0100

    style: remove trailing space

--- signinghub_api_client/client.py
+++ signinghub_api_client/client.py
@@ -33,7 +33,7 @@ class SigningHubSession(BaseUrlSession):
     def request(self, method, url, *args, **kwargs):
         response = super().request(method, url, *args, **kwargs)
         if response.status_code in (200, 201):
-            if "Content-Type" in response.headers: 
+            if "Content-Type" in response.headers:
                 if "application/json" in response.headers["Content-Type"]: # TODO: Proper mime-type parsing
                     return response.json()
                 if "application/octet-stream" in response.headers["Content-Type"]:

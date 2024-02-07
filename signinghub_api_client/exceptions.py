85de5cc1a8cdaf258d9a8d071627655f53ff63e7
Sergio Fenoll <sergio@fenoll.be>
2024-02-07 15:10:01 +0100

    chore: clean up SH exception printing a bit

--- signinghub_api_client/exceptions.py
+++ signinghub_api_client/exceptions.py
@@ -5,20 +5,28 @@ class SigningHubException(Exception):
     def __init__(self, response):
         super().__init__(response)
         self.response = response
+        self._response_truncated = False
+        self._print_fields = {}
+
         try:
             data = response.json()
             if "Message" in data:
                 self.error_description = data["Message"]
+                self._print_fields["error description"] = self.error_description
         except requests.exceptions.JSONDecodeError:
-            # Truncate the response text to 100K characters
-            # to prevent huge responses from overloading the service
-            self.error_description = response.text[:100000]
+            pass
+        # Truncate the response text to 100K characters
+        # to prevent huge responses from overloading the service
+        max_body_length = 10000
+        self._response_text = response.text[:max_body_length]
+        if len(response.text) > max_body_length:
+            self._response_truncated = True
 
     def __str__(self):
-        return """SigningHubException:
-error description: {error_description}""".format(
-            error_description=self.error_description
-        )
+        class_name = type(self).__name__
+        return f"""{class_name}
+{f"{k}: {v}" for k, v in self._print_fields.items()}
+response text {"(truncated)" if self._response_truncated else ""}: {self._response_text}"""
 
 
 class AuthenticationException(SigningHubException):
@@ -27,31 +35,19 @@ class AuthenticationException(SigningHubException):
         try:
             data = response.json()
             self.error_id = data["error"] if "error" in data else None
-            self.error_description = (
-                data["error_description"] if "error_description" in data else None
-            )
+            if "error" in data:
+                self.error_id = data["error"]
+                self._print_fields["error id"] = self.error_id
+            if "error_description" in data:
+                self.error_description = data["error_description"]
+                self._print_fields["error description"] = self.error_description
         except requests.exceptions.JSONDecodeError:
-            # Truncate the response text to 100K characters
-            # to prevent huge responses from overloading the service
-            self.error_description = response.text[:100000]
+            pass
         self.x_headers = {
             k: v for k, v in self.response.headers.items() if k.startswith("x-")
         }
-
-    def __str__(self):
-        return """SigningHub AuthenticationException:
-error: "{error_id}"
-error description: {error_description}
-x- headers: {x_headers}""".format(
-            error_id=self.error_id,
-            error_description=self.error_description,
-            x_headers=self.x_headers,
-        )
+        self._print_fields["x- headers"] = self.x_headers
 
 
 class UnauthenticatedException(SigningHubException):
-    def __str__(self):
-        return """SigningHub UnauthenticatedException:
-error description: {error_description}""".format(
-            error_description=self.error_description
-        )
+    pass
